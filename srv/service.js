ff2f0bc46699f0fe007f7edf4696a6c40c5781c6
Victor Hugo Cardoso <victor.cardoso@intelligenzait.com>
2024-11-27 03:45:31 +0000

    add `cust_ListadePresenca` entity CRUD

--- srv/service.js
+++ srv/service.js
@@ -14,14 +14,10 @@ module.exports = async (srv) => {
     async (req) => await successFactor.run(req.query)
   )
 
-  srv.on(
-    'UPDATE',
-    'cust_Turmas',
-    async (req) => {
-      const { externalCode, ...data } = req.data;
-      await successFactor.put(`/cust_Turmas('${externalCode}')`, data);
-    }
-  )
+  srv.on('UPDATE', 'cust_Turmas', async (req) => {
+    const { externalCode, ...data } = req.data
+    await successFactor.put(`/cust_Turmas('${externalCode}')`, data)
+  })
 
   srv.on(
     'DELETE',
@@ -42,7 +38,24 @@ module.exports = async (srv) => {
   )
 
   srv.on(
+    'CREATE',
+    'cust_ListadePresenca',
+    async (req) => await successFactor.run(req.query)
+  )
+
+  srv.on(
     'READ',
+    'cust_ListadePresenca',
+    async (req) => await successFactor.run(req.query)
+  )
+
+  srv.on('UPDATE', 'cust_ListadePresenca', async (req) => {
+    const { externalCode, ...data } = req.data
+    await successFactor.put(`/cust_ListadePresenca('${externalCode}')`, data)
+  })
+
+  srv.on(
+    'DELETE',
     'cust_ListadePresenca',
     async (req) => await successFactor.run(req.query)
   )
